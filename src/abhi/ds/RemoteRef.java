f57ace36dc4507594f258baae2f66bce55bfa5c7
Abhishek Sharma <abhisharma@cmu.edu>
2014-06-18 13:16:42 -0400

    Added Remote Reference

--- src/abhi/ds/RemoteRef.java
+++ src/abhi/ds/RemoteRef.java
@@ -7,7 +7,7 @@ package abhi.ds;
  * @author abhisheksharma
  *This class encapsulated the Remote REFERENCE> 
  *Which is nothing but the MetaData about the Remote Object. This meta-data will be used to basiclaly  create the STUB.
- *This meta is also pretyy much registered in the registry
+ *This meta is also pretty much registered in the registry
  */
 public class RemoteRef implements ISignal{
 	
