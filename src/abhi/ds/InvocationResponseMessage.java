d546eec52aced2378dca0f67fe3e0c662c541df9
Abhishek Sharma <abhisharma@cmu.edu>
2014-06-20 17:17:44 -0400

    Added the invocation handler

---
+++ src/abhi/ds/InvocationResponseMessage.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package abhi.ds;
+
+/**
+ * @author abhisheksharma
+ *
+ */
+public class InvocationResponseMessage extends BaseSignal {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Object returnObject;
+	
+
+	public void InvocationRepsonseMessage(Object object)
+	{
+		this.returnObject = object;
+		this.signalType = SignalType.InvocationResponse;
+	}
+	
+	public Object getReturnObject()
+	{
+		return this.returnObject;
+	}
+
+}
