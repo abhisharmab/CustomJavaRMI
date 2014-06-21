6e03f68c85cff4e946a492d58db69e5be6b8ba75
Abhishek Sharma <abhisharma@cmu.edu>
2014-06-20 21:01:28 -0400

    client

--- src/abhi/ds/SampleClient.java
+++ src/abhi/ds/SampleClient.java
@@ -8,5 +8,18 @@ package abhi.ds;
  *
  */
 public class SampleClient {
+	
+	public static void main(String[] args)
+	{
+		if(args.length !=2)
+		{
+			System.err.println("Usage: TestClient <registry_ip> <registry_port>");
+		    return; 	
+		}
+		
+		String registryIpAddr = args[0].trim();
+		int registryPort = Integer.parseInt(args[0].trim());
+		
+	}
 
 }
