925eb677057d5b20e2fca2c4cd6849c414aadcf9
dkrew <dkrew0213@gmail.com>
2014-06-20 22:17:23 -0400

    testing 2

--- src/abhi/ds/AckLookupSignal.java
+++ src/abhi/ds/AckLookupSignal.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 
 /**
- * @author abhisheksharma, dkrew0213
+ * @author abhisheksharma, dkrew0213asvasvs
  *This class encapsulated the Remote REFERENCE> 
  *Which is nothing but the MetaData about the Remote Object. This meta-data will be used to basiclaly  create the STUB.
  *This meta is also pretty much registered in the registry
