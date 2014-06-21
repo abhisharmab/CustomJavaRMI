736ac0c15a00261a561cd85853f625e2ac932ead
dkrew <dkrew0213@gmail.com>
2014-06-20 21:36:06 -0400

    bind signal

---
+++ src/abhi/ds/BindSignal.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package abhi.ds;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author abhisheksharma, dkrew0213
+ *This class encapsulated the remote REFERENCE> 
+ *Which is nothing but the MetaData about the remote Object. This meta-data will be used to basiclaly  create the STUB.
+ *This meta is also pretty much registered in the registry
+ */
+public class BindSignal extends BaseSignal{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private RemoteRef remote_Ref;
+	
+	public BindSignal(){
+		setSignalType(SignalType.Bind);
+	}
+
+	public RemoteRef getRemote_Ref() {
+		return remote_Ref;
+	}
+
+	public void setRemote_Ref(RemoteRef remote_Ref) {
+		this.remote_Ref = remote_Ref;
+	}
+	
+	
+
+
+}
